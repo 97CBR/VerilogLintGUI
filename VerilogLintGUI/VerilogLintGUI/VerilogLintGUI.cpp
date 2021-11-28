31ecfb5f0a212f1cf2412bdbbdc83d6c66b90e6f
MARXCBR <824368602@qq.com>
2021-11-28 15:08:07 +0800

    完成位状态读取demo

--- VerilogLintGUI/VerilogLintGUI/VerilogLintGUI.cpp
+++ VerilogLintGUI/VerilogLintGUI/VerilogLintGUI.cpp
@@ -1,8 +1,18 @@
 #include "VerilogLintGUI.h"
 
-VerilogLintGUI::VerilogLintGUI(QWidget *parent)
-    : QMainWindow(parent)
+bool isIndexBitRead(int index)
 {
-    ui.setupUi(this);
-    setWindowIcon(QIcon(":/VerilogLintGUI/Resource/icon.png"));
+	unsigned char data[4] = {0b00000000, 0b00100000, 0b00000000, 0b00000000}; // 4*8/2 = 16 根线，32个bit
+	int x = index * 2 / 8;  // index/ 4 确认具体组
+	int offset = index % 4; // 
+	qDebug("XXXXXXXXX = %d", (data[x] >> ((4 - offset) * 2)) & 0b01);
+	return true;
+}
+
+VerilogLintGUI::VerilogLintGUI(QWidget* parent)
+	: QMainWindow(parent)
+{
+	isIndexBitRead(6);
+	// ui.setupUi(this);
+	// setWindowIcon(QIcon(":/VerilogLintGUI/Resource/icon.png"));
 }
